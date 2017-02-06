3c26751997737388e916618ed51b2cc1b1e1141d
Anton <anton.arboleda2gmail.com>
2017-02-05 17:52:09 -0800

    Get server running

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ let contexts = [];
 
 const app = express();
 
-app.get('smssent', () => {
+// app.get('smssent', () => {
 
+// });
+
+app.listen(3000, () => {
+  console.log('Connected to port 3000');
 });
